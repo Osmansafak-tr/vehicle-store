47e84e0dcd8b92f197383a9892853d98e12f131e
koyuncufatih <122054311+koyuncufatih@users.noreply.github.com>
2023-12-25 14:20:01 +0300

    fetch_islemleri_yapıldı

--- appclient/src/views/userHome.vue
+++ appclient/src/views/userHome.vue
@@ -2,18 +2,23 @@
 
     <button class="btn btn-primary" @click="goToPage('/user/' + $route.params.id + '/add')">Add</button>
     <section class="grid-container">
-        <vehiclecard v-for="n in users.find(user => user.u_id == $route.params.id)?.number_of_car"
-                     :plate="vehicles.filter(vehicle => vehicle.user_id == $route.params.id)[n-1]?.plate"
-                     :src="models.find(model => model.id == vehicles.filter(vehicle => vehicle.user_id == $route.params.id)[n-1]?.model_id)?.img"
-                     :vid="vehicles.filter(vehicle => vehicle.user_id == $route.params.id)[n-1]?.v_id"
-                     >
+        <vehiclecard v-for="n in users2.find(user => user.u_id == $route.params.id)?.number_of_car"
+                     :plate="vehicles2.filter(vehicle => vehicle.user_id == $route.params.id)[n-1]?.plate"
+                     :src="models.find(model => model.id == vehicles2.filter(vehicle => vehicle.user_id == $route.params.id)[n-1]?.model_id)?.img"
+                     :vid="vehicles2.filter(vehicle => vehicle.user_id == $route.params.id)[n-1]?.v_id">
         </vehiclecard>
+        <vehiclecard v-for="n in number_of_cars"
+                     :plate="vehicles.filter(vehicle => vehicle.userId == $route.params.id)[n-1]?.plate"
+                     :src="vehicleModels.find(model => model.id == vehicles.filter(vehicle => vehicle.userId == $route.params.id)[n-1]?.modelId)?.imageUrl"
+                     :vid="vehicles.filter(vehicle => vehicle.userId == $route.params.id)[n-1]?.vin"></vehiclecard>
+                     
     </section>
 
 </template>
 
 <script>
     import Vehiclecard from "@/components/Vehiclecard.vue";
+    import axios from 'axios';
 
     export default {
         components: {
@@ -29,9 +34,18 @@
         data() {
             return {
                 users: [],
+                users2: [],
                 vehicles: [],
+                vehicles2: [],
                 models: [],
+                vehicleModels: [],
+                number_of_cars: null,
             }
+        },
+        mounted() {
+            this.fetchVehicles();
+            this.fetchUsers();
+            this.fetchVehicleModels();
         },
 
         methods: {
@@ -41,21 +55,52 @@
             async getUsers() {
                 let res = await fetch('users.json');
                 let data = await res.json();
-                    
-                this.users = data;
+
+                this.users2 = data;
             },
             async getVehicles() {
                 let res = await fetch('vehicles.json');
                 let data = await res.json();
 
-                this.vehicles = data;
+                this.vehicles2 = data;
             },
+
+            async fetchVehicles() {
+                try {
+                    const response = await axios.get('https://localhost:7083/Vehicles');
+                    this.vehicles = response.data;                    
+                    this.number_of_cars = this.vehicles.filter(vehicle => vehicle.userId == this.$route.params.id).length;
+                } catch (error) {
+                    console.error('Error fetching vehicles:', error);
+                }
+            },
+
+            async fetchUsers() {
+                try {
+                    const response = await axios.get('https://localhost:7083/Users');
+                    this.users = response.data;
+                    const test = this.users.find(user => user.id == this.$route.params.id)?.name;
+
+                } catch (error) {
+                    console.log('Error fetching users:', error);
+                }
+            },
+
+            async fetchVehicleModels() {
+                try {
+                    const response = await axios.get('https://localhost:7083/VehicleModels');
+                    this.vehicleModels = response.data;                    
+                } catch (error) {
+                    console.log('Error fetching users:', error);
+                }
+            },
+
             async getModels() {
                 let res = await fetch('models.json');
                 let data = await res.json();
-                    
+
                 this.models = data;
-               
+
             },
 
         },
